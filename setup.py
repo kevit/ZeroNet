7cea68e4896f7417eb906afd3d9765e2448cbbb8
kevit <root@kevit.info>
2019-04-16 13:46:19 +0200

    initial setup.py version

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ZeroNet',
+      version='1.0',
+      description='ZeroNet python package',
+      author='kevit',
+      author_email='root@kevit.info',
+      url='zeronet',
+      packages=['distutils', 'distutils.command'],
+     )
